ffd7943b090d70b001e002be211de778ae7768c5
huweili <czxyhuweili@163.com>
2026-03-16 21:47:29 +0800

    综合管理区

--- src/views/three/threeController.vue
+++ src/views/three/threeController.vue
@@ -54,6 +54,7 @@
           <button @click="toTargetRoom('generatorRoom')" class="control-btn">有线通信室</button>
           <button @click="toTargetRoom('chemicalPreventionDutyRoom')" class="control-btn">防化值班室</button>
           <button @click="toTargetRoom('microwaveSatelliteCommunicationRoom')" class="control-btn">微波卫星通信室</button>  
+          <button @click="toTargetRoom('managementAreaRoom')" class="control-btn">综合管理区</button>  
         </div>
       </div>
 
