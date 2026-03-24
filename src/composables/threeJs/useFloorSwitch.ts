a13f105c17b679f7ce554aa37c57a259910ea9b9
huweili <czxyhuweili@163.com>
2026-03-24 18:51:14 +0800

    冗余代码删除

--- src/composables/threeJs/useFloorSwitch.ts
+++ src/composables/threeJs/useFloorSwitch.ts
@@ -33,8 +33,6 @@ export function useFloorSwitch(
   ) => {
     const characterModelUrl = basisStore.characterModelUrlsConfig?.man || ''
     
-    debugger
-    
     // 1、先移除人物模型
     removeModel(characterModelUrl)
 
