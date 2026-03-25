37e71dc0e7b1880ca2ec62aa0b9e53dfccac532e
huweili <czxyhuweili@163.com>
2026-03-25 10:43:54 +0800

    按钮精灵出弹窗

--- src/components/threeJs/index.vue
+++ src/components/threeJs/index.vue
@@ -119,7 +119,11 @@ const initInteractions = () => {
           object
         })
 
-        threeJsStore.toFloor(object.userData.id) // 双击精灵模型后，切换到指定楼层
+        if(object.userData?.type !== 'button') {
+          threeJsStore.toFloor(object.userData.id) // 双击精灵模型后，切换到指定楼层
+        }else{
+          console.log('出弹窗')
+        }
       }
     },
     highlightEnabled: true
